d1cf1f0c6aeb5f86ca1f1577213ddd524a8904cf
monisha-ncompass <monisha@ncompass.in>
2021-12-16 18:47:16 +0530

    changed

--- test123/index.js
+++ test123/index.js
@@ -1,4 +1,5 @@
 exports.handler =  async function(event, context) {
   console.log("EVENT: \n" + JSON.stringify(event, null, 2))
+  console.log("Siddhant purushothaman harsha hepzibah")
   return context.logStreamName
 }
