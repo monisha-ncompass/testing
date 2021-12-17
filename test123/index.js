65f8f2b9e8c2755074d800680a415fd4d91b7cac
monisha-ncompass <monisha@ncompass.in>
2021-12-17 20:45:51 +0530

    response

--- test123/index.js
+++ test123/index.js
@@ -2,5 +2,8 @@ exports.handler =  async function(event, context) {
   console.log("EVENT: \n" + JSON.stringify(event, null, 2))
   console.log("Siddhant purushothaman harsha hepzibah")
   console.log("hellooooo")
-  return context.logStreamName
-}
+  return {
+    statusCode: 200,
+    message: context.logStreamName
+  }
+ }
