a46cb7b25c14df03c3e9d9fa62ee40eea0fbee57
monisha-ncompass <monisha@ncompass.in>
2021-12-17 18:35:23 +0530

    gateway

---
+++ build/api_gateway_deploy.sh
@@ -0,0 +1,41 @@
+cat >gateway_template.yaml <<EOM
+AWSTemplateFormatVersion: 2010-09-09
+Description: Lambda and API Gateway
+
+Resources:
+RestApi:
+    Type: 'AWS::ApiGateway::RestApi'
+    Properties:
+      Name: test_api
+
+
+
+testMethod:
+    Type: 'AWS::ApiGateway::Method'
+    Properties:
+      RestApiId: !Ref RestApi
+      HttpMethod: GET
+      Integration:
+        Type: AWS_PROXY
+        IntegrationHttpMethod: POST
+        Uri: !ImportValue 'testingARN'
+        IntegrationResponses:
+          - ResponseTemplates:
+              application/json: ""
+            StatusCode: 200
+        PassthroughBehavior: WHEN_NO_TEMPLATES
+      MethodResponses:
+        - StatusCode: 200
+          ResponseModels: { "application/json": "Empty" }     
+
+
+Deployment:
+    Type: 'AWS::ApiGateway::Deployment'
+    DependsOn: testMethod
+    Properties:
+      RestApiId: !Ref RestApi
+  
+
+EOM
+
+cat gateway_template.yaml
