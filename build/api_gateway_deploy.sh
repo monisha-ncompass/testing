6404ccc9e4ea1c9aa4671058aa57a8646b551085
monisha-ncompass <monisha@ncompass.in>
2021-12-17 19:33:35 +0530

    ind5

--- build/api_gateway_deploy.sh
+++ build/api_gateway_deploy.sh
@@ -10,30 +10,30 @@ Resources:
         
     testMethod:
         Type: 'AWS::ApiGateway::Method'
-        Properties:
-        RestApiId: !Ref RestApi
-        HttpMethod: GET
-        Integration:
-            Type: AWS_PROXY
-            IntegrationHttpMethod: POST
-            Uri: !ImportValue 'testingARN'
-            IntegrationResponses:
-            - ResponseTemplates:
-                application/json: ""
-                StatusCode: 200
-            PassthroughBehavior: WHEN_NO_TEMPLATES
-        MethodResponses:
-        - StatusCode: 200
-          ResponseModels: { "application/json": "Empty" }     
+        Properties: 
+            RestApiId: !Ref RestApi
+            HttpMethod: GET
+            Integration:
+                Type: AWS_PROXY
+                IntegrationHttpMethod: POST
+                Uri: !ImportValue 'testingARN'
+                IntegrationResponses:
+                    - ResponseTemplates:
+                        application/json: ""
+                      StatusCode: 200
+                PassthroughBehavior: WHEN_NO_TEMPLATES
+            MethodResponses:
+                - StatusCode: 200
+                  ResponseModels: { "application/json": "Empty" }     
 
 
     Deployment:
         Type: 'AWS::ApiGateway::Deployment'
         DependsOn: testMethod
         Properties:
-        RestApiId: !Ref RestApi
-        Description: TEST STAGE
-        StageName: DEV
+            RestApiId: !Ref RestApi
+            Description: TEST STAGE
+            StageName: DEV
   
 
 EOM
