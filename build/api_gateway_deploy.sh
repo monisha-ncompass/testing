82a2cc827208ee917713ab6de7fdb6925a0b58c8
monisha-ncompass <monisha@ncompass.in>
2021-12-17 19:02:31 +0530

    indentation

--- build/api_gateway_deploy.sh
+++ build/api_gateway_deploy.sh
@@ -3,37 +3,37 @@ AWSTemplateFormatVersion: 2010-09-09
 Description: Lambda and API Gateway
 
 Resources:
-RestApi:
-    Type: 'AWS::ApiGateway::RestApi'
-    Properties:
-      Name: test_api
+    RestApi:
+        Type: 'AWS::ApiGateway::RestApi'
+        Properties:
+        Name: test_api
+        
+    testMethod:
+        Type: 'AWS::ApiGateway::Method'
+        Properties:
+        RestApiId: !Ref RestApi
+        HttpMethod: GET
+        Integration:
+            Type: AWS_PROXY
+            IntegrationHttpMethod: POST
+            Uri: !ImportValue 'testingARN'
+            IntegrationResponses:
+            - ResponseTemplates:
+                application/json: ""
+                StatusCode: 200
+            PassthroughBehavior: WHEN_NO_TEMPLATES
+        MethodResponses:
+            - StatusCode: 200
+            ResponseModels: { "application/json": "Empty" }     
 
 
-
-testMethod:
-    Type: 'AWS::ApiGateway::Method'
-    Properties:
-      RestApiId: !Ref RestApi
-      HttpMethod: GET
-      Integration:
-        Type: AWS_PROXY
-        IntegrationHttpMethod: POST
-        Uri: !ImportValue 'testingARN'
-        IntegrationResponses:
-          - ResponseTemplates:
-              application/json: ""
-            StatusCode: 200
-        PassthroughBehavior: WHEN_NO_TEMPLATES
-      MethodResponses:
-        - StatusCode: 200
-          ResponseModels: { "application/json": "Empty" }     
-
-
-Deployment:
-    Type: 'AWS::ApiGateway::Deployment'
-    DependsOn: testMethod
-    Properties:
-      RestApiId: !Ref RestApi
+    Deployment:
+        Type: 'AWS::ApiGateway::Deployment'
+        DependsOn: testMethod
+        Properties:
+        RestApiId: !Ref RestApi
+        Description: TEST STAGE
+        StageName: DEV
   
 
 EOM
