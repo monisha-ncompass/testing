0d8c424c41375595deb629d9c0cd4f2bd62b83f1
monisha-ncompass <monisha@ncompass.in>
2021-12-21 16:47:06 +0530

    put method

--- test123/index.js
+++ test123/index.js
@@ -2,6 +2,17 @@ exports.handler =  async function(event, context) {
   console.log("EVENT: \n" + JSON.stringify(event, null, 2))
   console.log("Siddhant purushothaman harsha hepzibah")
   console.log("hellooooo")
+  if(event.httpMethod === 'PUT')
+  {
+    return {
+      statusCode: 200,
+      headers:{  "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*",
+      "Content-Type": "application/json"},
+      body: "put method for cors test"
+    }
+  }
+
   return {
     statusCode: 200,
     headers:{    "Access-Control-Allow-Origin": "*",
