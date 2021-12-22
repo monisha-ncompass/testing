b22149102ea7a8f05b6eacbd0fe907406629626b
monisha-ncompass <monisha@ncompass.in>
2021-12-22 17:29:07 +0530

    jwt verification

--- api-authorizer/index.js
+++ api-authorizer/index.js
@@ -1,16 +1,51 @@
-exports.handler = async (event) => {
+const jwt = require('jsonwebtoken');
+exports.handler = async (event,callback) => {
     // TODO implement
-    
-    const response = {
-        "principalId": "apigateway.amazonaws.com",
+
+    let response = {
+        "principalId": null, 
         "policyDocument": {
-            "Version": "2012-10-17",
-            "Statement": [{
-                "Action": "execute-api:Invoke",
-                "Effect": "Allow",
-                "Resource": event.methodArn
+          "Version": "2012-10-17",
+          "Statement": [
+            {
+              "Action": "execute-api:Invoke",
+              "Effect": "Deny",
+              "Resource": "*",
             }]
         }
-    };
+      };
+
+    let verified = jwt.verify(token, 'secret-key', function(err, decoded) {
+        if(err){
+            console.log(err); 
+            return false
+        }
+
+       if(validateEmail(decoded.email)){
+            response = {
+                "principalId": "apigateway.amazonaws.com",
+                "policyDocument": {
+                    "Version": "2012-10-17",
+                    "Statement": [{
+                        "Action": "execute-api:Invoke",
+                        "Effect": "Allow",
+                        "Resource": event.methodArn
+                    }]
+                }
+            };
+            return true
+        }  
+ });
+    if(!verified)
+    {
+        callback("Unauthorized")
+        return
+    }
     return response;
+};
+
+const validateEmail = (email) => {
+  return email.match(
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
 };
